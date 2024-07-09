7fdca4ea32b007f97910f28bd20b9f1fb29e98ec
ridaprivt <ridaofficial88@gmail.com>
2024-07-09 14:25:48 +0500

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,26 @@
+import 'package:amplify_analytics_pinpoint/amplify_analytics_pinpoint.dart';
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
+import 'package:amplify_core/amplify_core.dart';
 import 'package:flutter/material.dart';
+import 'amplifyconfiguration.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await configureAmplify();
+  runApp(MyApp());
+}
+
+Future<void> configureAmplify() async {
+  final auth = AmplifyAuthCognito();
+  final analytics = AmplifyAnalyticsPinpoint();
+
+  try {
+    Amplify.addPlugins([auth, analytics]);
+
+    await Amplify.configure(amplifyconfig);
+  } on AmplifyAlreadyConfiguredException catch (e) {
+    print(e);
+  }
 }
 
 class MyApp extends StatelessWidget {
